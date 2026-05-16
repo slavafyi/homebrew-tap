db70ba74e5e0597bbe5513e43902d30d283f4146
Slava M. <mailer@noreply.slava.fyi>
2026-05-16 17:44:43 +0300

    fix: satisfy cask style

--- Casks/telegram@12.6.rb
+++ Casks/telegram@12.6.rb
@@ -2,7 +2,7 @@ cask "telegram@12.6" do
   version "12.6,279903"
   sha256 "b8a09a297a4b72ea4ea4de1ac71ded507f4215c0f443c02f897c04966fce2ebe"
 
-  url "https://osx.telegram.org/updates/Telegram-#{version.before_comma}.#{version.after_comma}.app.zip"
+  url "https://osx.telegram.org/updates/Telegram-#{version.csv.first}.#{version.csv.second}.app.zip"
   name "Telegram for macOS"
   desc "Messaging app with a focus on speed and security"
   homepage "https://macos.telegram.org/"
@@ -13,8 +13,9 @@ cask "telegram@12.6" do
 
   conflicts_with cask: "telegram"
 
+  depends_on :macos
+
   app "Telegram.app"
 
   uninstall quit: "ru.keepcoder.Telegram"
 end
-
